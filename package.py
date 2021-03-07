2dabe7732ed7733d0da33c4b4f1ea581196960f4
Chandan Prasad M <prasadchandan@gmail.com>
2021-03-07 12:25:30 -0500

    Update package.py to create subdirectory in file

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 FILES = (
     "__init__.py",
@@ -10,12 +10,12 @@ FILES = (
 )
 
 git_revision = subprocess.check_output(("git", "rev-parse", "--short", "HEAD")).strip().decode()
-output_file_name = os.path.join('dist', f'blender_coiled_spring_{git_revision}.zip')
+output_file_name = os.path.join('dist', f'coiled_spring_{git_revision}.zip')
 
 print(f'Creating blender addon archive {output_file_name}')
 
 with ZipFile(output_file_name, "x") as zf:
     for f in FILES:
-        zf.write(f)
+        zf.write(f, os.path.join("coiled_spring", f'{f}'), ZIP_DEFLATED)
 
 print(f'Done')
